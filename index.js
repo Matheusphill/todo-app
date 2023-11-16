bce5ba1d889e79abca009ab72e49d6377fa5df7f
Matheus <omatheusphilipe@gmail.com>
2023-11-16 10:33:04 -0300

    primeira execuçao do servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require("express")
 
 const app = express()
+
+app.get('/',(requisiçao, resposta)=> {
+    resposta.send("Olá, mundo!")
+})
+
+app.listen(3000,()=>{
+    console.log("servidor rodando na porta 3000")
+})
